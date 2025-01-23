9c694432ed78a85d7c33e64b4a8c50bafcbe7d81
Harsh Thakare <thakareharsh03@gmail.com>
2025-01-23 16:00:51 +0530

    Add files via upload

---
+++ MY SQL DAY 1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE my_first_database;
+USE my_first_database;
+CREATE TABLE users(
+id INT auto_increment primary KEY,
+name varchar(50),
+email varchar(50)
+);
+select*from users;
+insert INTO users(name,email)values('harsha','harsha@gmail.com');
+select*from users;
+insert INTO users(name,email)values('dipali','dipali@gmail.com');
+select*from users;
+insert INTO users (name,email)values('ruchika','ruchika@gmail.com');
+select*from users;
+insert INTO users(name,email)values('sonu','sonu@gmail.com');
+select*from users;
+insert INTO users (name,email)value('sharda','sharda@gmail.com');
+select*from users;
+insert INTO users (name,email)value('pranay','pranay@gmail.com');
+select*from users;
+insert INTO users(name,email)value('dhairya','dhairya@gmail.com');
+select*from users;
+insert INTO users(name,email)value('rina','rina@gmail.com');
+select*from users;
+insert INTO users (name,email)value('tanya','tanaya@gmail.com');
+select*from users;
+insert INTO users (name, email) value('samir','samir@gmail.com');
+select*from users;
+insert INTO users(name,email)value('rushi','rushi@gmail.com');
+select*from users;
